d75e53a6360f1db68ddad3eeca176916a1358414
Parinvut Rochanavedya <jmxz_1995@hotmail.com>
2015-03-12 14:23:10 +0700

    Removed Hello World Scene

--- main.js
+++ main.js
@@ -47,9 +47,12 @@
  *
  */
 
+ screenWidth = 700;
+ screenHeight = 600;
+
 cc.game.onStart = function(){
     cc.view.adjustViewPort(true);
-    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(screenWidth, screenHeight, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
